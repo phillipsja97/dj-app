e9298fb3c4d53cb28f6a89996e7dcdbbc0e7db7f
Jamie <phillipsja97@gmail.com>
2020-04-11 19:18:24 -0500

    file setup

--- App.js
+++ App.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
-import { ImageBackground, View, Text, StyleSheet, Image, Alert } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Left, Right, Body, Button, Icon } from 'native-base';
+// import React, { Component } from 'react';
+// import { ImageBackground, View, Text, StyleSheet, Image, Alert } from 'react-native';
+// import { Container, Header, Title, Content, Footer, FooterTab, Left, Right, Body, Button, Icon } from 'native-base';
+// import Navigator from './routes/Homestack.js';
 
-const image = { uri: "https://images2.alphacoders.com/701/thumb-1920-70172.jpg" };
 
-const styles = StyleSheet.create ({
-  text: {
-    marginTop: 200,
-    position: 'absolute',
-    alignItems: 'center',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  button: {
-    width: '85%',
-    justifyContent: 'center'
-  }
-})
+// export default function App() {
+//     return (
+//       <Navigator />
+//     );
+//   }
 
-export default function AnatomyExample() {
-    return (
-       <ImageBackground source={image} style={{ height: '100%', width: '100%' }}>
-       <View style={styles.text} >
-       <Image source={{
-         uri: 'https://www.logofury.com/blog/wp-content/uploads/2018/12/DJ-Logo-Idea-1.jpg'
-        }} 
-        style={{ height: 200, width: 200, marginBottom: 50 }} 
-        />
-          <View style={{ justifyContent: 'center' }}>
-          <Button full danger iconRight style={styles.button}
-          onPress={() => alert("You logged In!")}
-          >
-              <Text style={{ paddingRight: 30 }}>Sign In With Google</Text>
-              <Icon type= "AntDesign" name="google"></Icon>
-          </Button>
-          </View>
-       </View>
-       </ImageBackground>
-    );
-  }
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './Homescreen';
+import Profile from './Profile';
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//     </View>
+//   );
+// }
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
---
+++ Homestack.js
@@ -0,0 +1,15 @@
+// import { createStackNavigator } from 'react-navigation/stack';
+// import { createAppContainer } from 'react-navigation/native';
+
+// const screens = {
+//   Home: {
+//     screen: Homescreen
+//   },
+//   Profile: {
+//     screen: Profile
+//   },
+// }
+
+// const Homestack = createStackNavigator(screens);
+
+// export default createAppContainer(Homestack);
